a3cc777b86cb88cc88aeaad19c64f04a28769d32
Marcel Schwittlick <mar@archimedes-exhibitions.de>
2016-09-22 16:37:27 +0200

    fixes the word_level_rnn wrapper and implementation. relates to #103

--- model.py
+++ model.py
@@ -61,7 +61,7 @@ class Model():
         self.train_op = optimizer.apply_gradients(zip(grads, tvars))
 
     def sample(self, sess, words, vocab, num=200, prime='first all', sampling_type=1):
-        state = self.cell.zero_state(1, tf.float32).eval()
+        state = self.cell.zero_state(1, tf.float32).eval(session=sess)
         print ('prime: ' + prime)
         for word in [prime]:
             print (word)
