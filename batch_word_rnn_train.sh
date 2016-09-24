0b31e04289d4bdc5d1091b5e3f9b955b9748b402
Marcel Schwittlick <marzzzel@gmail.com>
2016-09-24 21:14:09 +0200

    adds example for batch training

--- batch_word_rnn_train.sh
+++ batch_word_rnn_train.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-python word_level_rnn/train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/wark_mckenzie-a_hacker_manifesto.txt --save_dir ./word_level_rnn/saved/wark_mckenzie-a_hacker_manifesto --model lstm --save_every 100
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/warren_sack-aesthetics_of_information_visualization.txt --save_dir ./saved/warren_sack-aesthetics_of_information_visualization --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/va-cyposium.txt --save_dir ./saved/va-cyposium --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/tatiana_bazzichelli_geoff_cox-disrupting_business_art_and_activism_in_times_of_financial_crisis.txt --save_dir ./saved/tatiana_bazzichelli_geoff_cox-disrupting_business_art_and_activism_in_times_of_financial_crisis --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/simulation.txt --save_dir ./saved/simulation --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/sherry_turkle-the_second_self_computers_and_the_human_spirit.txt --save_dir ./saved/sherry_turkle-the_second_self_computers_and_the_human_spirit --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/sherry_turkle-how_computers_change_the_way_we_think.txt --save_dir ./saved/sherry_turkle-how_computers_change_the_way_we_think --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/roy_ascott-telematic_embrace_visionary_theories_of_art_technology_and_consciousness.txt --save_dir ./saved/roy_ascott-telematic_embrace_visionary_theories_of_art_technology_and_consciousness --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/roy_ascott-behaviousist_art_and_the_cybernetic_vision.txt --save_dir ./saved/roy_ascott-behaviousist_art_and_the_cybernetic_vision --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/rosa_menkman_the_glitch_momentum.txt --save_dir ./saved/rosa_menkman_the_glitch_momentum --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/richard_barbrook_andy_cameron-the_internet_revolution_from_dotcom_capitalism_to_cybernetic_communism.txt --save_dir ./saved/richard_barbrook_andy_cameron-the_internet_revolution_from_dotcom_capitalism_to_cybernetic_communism --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/peter_sloterdijk-rules_for_the_human_zoo.txt --save_dir ./saved/peter_sloterdijk-rules_for_the_human_zoo --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/peter_sloterdijk-critique_of_cynical_reason.txt --save_dir ./saved/peter_sloterdijk-critique_of_cynical_reason --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/nick_bostrom-the_ethics_of_artificial_intelligence.txt --save_dir ./saved/nick_bostrom-the_ethics_of_artificial_intelligence --model lstm
+
+python train.py --data_dir data/ --train_filename /mnt/drive1/data/eco/pdf_to_text_postprocessed/neal_stephenson-in_the_beginning_was_the_command_line.txt --save_dir ./saved/neal_stephenson-in_the_beginning_was_the_command_line --model lstm
