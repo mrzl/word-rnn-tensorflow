05246e540ef7a55c9dbd09a77b24fed7774712d8
Marcel Schwittlick <marzzzel@gmail.com>
2016-10-02 21:44:28 +0200

    fiddling with tensorflow stuff. and adding another sampling function

--- word_lstm_wrapper.py
+++ word_lstm_wrapper.py
@@ -18,8 +18,8 @@ class WordLevelLSTM(object):
             self.saved_args = cPickle.load(f)
         with open(os.path.join(self.load_dir, 'words_vocab.pkl'), 'rb') as f:
             self.words, self.vocab = cPickle.load(f)
-
-        model = Model(self.saved_args, infer=False, varscope=self.varscope)
+        tf.reset_default_graph()
+        model = Model(self.saved_args, infer=True, varscope=self.varscope)
         vars = [k for k in tf.all_variables() if k.name.startswith(self.varscope)]
         with tf.Session() as sess:
             saver = tf.train.Saver(vars)
@@ -27,5 +27,23 @@ class WordLevelLSTM(object):
             if ckpt and ckpt.model_checkpoint_path:
                 saver.restore(sess, ckpt.model_checkpoint_path)
             answer = model.sample(sess, self.words, self.vocab, output_length, input, sample)
+            sess.close()
+            return answer
+
+    def sample2(self, input, sample=1, output_length=200):
+        print('Loading from ' + self.load_dir)
+        with open(os.path.join(self.load_dir, 'config.pkl'), 'rb') as f:
+            self.saved_args = cPickle.load(f)
+        with open(os.path.join(self.load_dir, 'words_vocab.pkl'), 'rb') as f:
+            self.words, self.vocab = cPickle.load(f)
+        tf.reset_default_graph()
+        model = Model(self.saved_args, infer=True, varscope=self.varscope)
+        vars = [k for k in tf.all_variables() if k.name.startswith(self.varscope)]
+        with tf.Session() as sess:
+            saver = tf.train.Saver(vars)
+            ckpt = tf.train.get_checkpoint_state(self.load_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+            answer = model.sample2(sess, self.words, self.vocab, output_length, input, sample)
             sess.close()
             return answer
